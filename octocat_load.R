e75c1a0f96fa97e9167f5dacca5a2e3d45cb2aed
Tint Tha Ra Wun <ttharawun@smith.edu>
2023-01-30 10:22:09 -0500

    Change from online to local file

--- octocat_load.R
+++ octocat_load.R
@@ -1,5 +1,5 @@
 # Code to load in Octocat (github mascot) art
 
-octocat = readLines("https://raw.githubusercontent.com/Adv-R-Programming/Adv-R-Reader/main/content/class_worksheets/02_git/octocat.txt")
+octocat = readLines("./octocat.txt")
 
 writeLines(octocat, "./octocat.txt")
